df2d82ffc3951cd53142b7293438f66f3667a7db
dp <davepollan@gmail.com>
2020-01-28 00:17:50 -0500

    basic HTML template done

--- index.js
+++ index.js
@@ -19,9 +19,21 @@ inquirer
 ])
  
 .then(({username}) => {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    const queryUrl = `https://api.github.com/users/${username}`;
     axios.get(queryUrl).then(res => {
-       console.log(res)
+      // console.log(res.data);
+       console.log(res.data.name);
+       console.log(res.data.html_url);
+       console.log(res.data.blog);
+       console.log(res.data.location);
+       console.log(res.data.bio);
+       console.log(res.data.avatar_url);
+
+       console.log(res.data.public_repos);
+       console.log(res.data.followers);
+       console.log(res.data.following);
+       console.log(res.data.starred_url);
+
         const repoNames = res.data.map(repo => repo.name + "\n");
         fs.writeFile("repos.txt", repoNames, (err, data) => {
             if (err) {
