923fd5bcb10a4ffa03ed493b69f8c2a409c1a866
dp <davepollan@gmail.com>
2020-01-28 22:01:55 -0500

    object data passing functions working

--- index.js
+++ index.js
@@ -3,22 +3,32 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 const questions = ['Enter your GitHub username', "What is your favorite color?"];
 
-inquirer
-   .prompt([
-   {
-       type: "input",
-       message: questions[0],
-       name: "username"
-    }
-    ,
-   {
-       type: "input",
-       message: questions[1],
-       name: "color"
-   }
-])
- 
-.then(({username}) => {
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "username"
+         }
+         ,
+        {
+            type: "list",
+            message: questions[1],
+            choices: ["green", "red", "blue", "pink"],
+            name: "color"
+        }
+        
+    ]).then(data => {
+        const {
+            username,
+            color
+        } = data;
+        getGihubInfo(username)
+    }).catch(err => console.log(err))
+  }
+
+  function getGihubInfo(username) {
     const queryUrl = `https://api.github.com/users/${username}`;
     axios.get(queryUrl).then(res => {
       // console.log(res.data);
@@ -34,29 +44,22 @@ inquirer
        console.log(res.data.following);
        console.log(res.data.starred_url);
 
-        const repoNames = res.data.map(repo => repo.name + "\n");
-        fs.writeFile("repos.txt", repoNames, (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(`You saved ${repoNames.length}`);
+       const pdf = require("./generateHTML.js");
+
+       promptUser()
+        .then(function(answers) {
+            const newFile = generateHTML(answers);
+
+            return writeFileAsync("index.html", html);
+        })
+        .then(function() {
+            console.log("Successfully wrote to index.html");
+        })
+        .catch(function(err) {
+            console.log(err);
         });
+
+
     });
-});
-
-/*
-const questions = [
-  
-];
-
-function writeToFile(fileName, data) {
- 
-}
-
-function init() {
-
-init();
-
-}
-
-*/
+};
+promptUser();
